0338be92f18dc26ab64aa4cd8807f2005eaaafe7
agauyeung <agauyeung@dones.usfca.edu>
2015-04-10 00:46:25 -0700

    functionDef and allocation left

--- assembly/AATBuildTree.java
+++ assembly/AATBuildTree.java
@@ -9,8 +9,19 @@ public class AATBuildTree {
     }
     
     public AATStatement ifStatement(AATExpression test, AATStatement ifbody, AATStatement elsebody) {
-	return null;
-    }
+        Label ifendlabel = new Label("ifend");
+        Label iftruelabel = new Label("iftrue");
+        AATLabel ifendtree = new AATLabel(ifendlabel);
+        AATLabel iftruetree = new AATLabel(iftruelabel);
+        
+        AATStatement tree = sequentialStatement(ifbody, ifendtree);
+        tree = sequentialStatement(iftruetree, tree);
+        tree = sequentialStatement(new AATJump(ifendlabel), tree);
+        if (elsebody != null) {
+            tree = sequentialStatement(elsebody, tree);
+        }        
+        return sequentialStatement(new AATConditionalJump(test, iftruelabel), tree);
+    }   /* DONE */
     
     /**
      * Allocate creates an AAT that makes a call to the built-in function allocate, which takes as 
@@ -25,17 +36,67 @@ public class AATBuildTree {
     }
 
     public AATStatement whileStatement(AATExpression test, AATStatement whilebody) {
-	return null;
-    }
+	Label whiletestlabel = new Label("whiletest");
+	Label whilestartlabel = new Label("whilestart");
+	AATLabel whiletesttree = new AATLabel(whiletestlabel);
+	AATLabel whilestarttree = new AATLabel(whilestartlabel);
+	
+	AATStatement tree = sequentialStatement(whiletesttree, new AATConditionalJump(test, whilestartlabel));
+	tree = sequentialStatement(whilebody, tree);
+	tree = sequentialStatement(whilestarttree, tree);
+	return sequentialStatement(new AATJump(whiletestlabel), tree);
+    }   /* DONE */
 
+    /**
+     * doWhileStart:
+     *          dowhilebody
+     * doWhileTest:
+     *          if (test) goto doWhileStart
+     *          
+     * @param test
+     * @param dowhilebody
+     * @return
+     */
     public AATStatement dowhileStatement(AATExpression test, AATStatement dowhilebody) {
-	return null;
-    }
+        Label dowhiletestlabel = new Label("dowhiletest");
+        Label dowhilestartlabel = new Label("dowhilestart");
+        AATLabel dowhiletesttree = new AATLabel(dowhiletestlabel);
+        AATLabel dowhilestarttree = new AATLabel(dowhilestartlabel);
+        
+        AATStatement tree = sequentialStatement(dowhiletesttree, new AATConditionalJump(test, dowhilestartlabel));
+        tree = sequentialStatement(dowhilebody, tree);
+        return sequentialStatement(dowhilestarttree, tree);
+    }   /* DONE */
   
+    /**
+     *          initialize
+     *          goto FORTEST
+     * FORSTART:
+     *          statement
+     *          increment
+     * FORTEST:
+     *          if (test) goto FORSTART 
+     * 
+     * @param init
+     * @param test
+     * @param increment
+     * @param body
+     * @return
+     */
     public AATStatement forStatement(AATStatement init, AATExpression test, 
 				     AATStatement increment, AATStatement body) {
-	return null;
-    }
+        Label fortestlabel = new Label("fortest");
+        Label forstartlabel = new Label("forstart");
+        AATLabel fortesttree = new AATLabel(fortestlabel);
+        AATLabel forstarttree = new AATLabel(forstartlabel);
+        
+        AATStatement tree = sequentialStatement(fortesttree, new AATConditionalJump(test, forstartlabel));
+        tree = sequentialStatement(increment, tree);
+        tree = sequentialStatement(body, tree);
+        tree = sequentialStatement(forstarttree, tree);
+        tree = sequentialStatement(new AATJump(fortestlabel), tree);
+        return sequentialStatement(init, tree);
+    }   /* DONE */
     
     public AATStatement emptyStatement() {
 	return new AATEmpty();
@@ -64,12 +125,15 @@ public class AATBuildTree {
     public AATExpression arrayVariable(AATExpression base,
 				       AATExpression index,
 				       int elementSize) {
-	return null;
-    }
+	AATExpression offset = new AATOperator(constantExpression(elementSize), index, AATOperator.MULTIPLY);
+	return new AATMemory(new AATOperator(base, offset, AATOperator.MINUS));
+    }   /* DONE */
     
     public AATExpression classVariable(AATExpression base, int offset) {
-	return null;
-    }
+	return new AATMemory(new AATOperator(base, 
+	                                    constantExpression(offset),        //offset is known at compile time
+	                                    AATOperator.MINUS));
+    }   /* DONE */
   
     public AATExpression constantExpression(int value) {
 	return new AATConstant(value);
@@ -90,9 +154,8 @@ public class AATBuildTree {
         AATMove move = new AATMove(new AATRegister(Register.Result()), value);
         //jump to label
         AATJump jump = new AATJump(functionend);
-        //TODO: Now what? Is it Sequential? or...? Or would we use AATReturn?
         return sequentialStatement(move, jump);
-    }
+    }   /* DONE */
 }
 
 
